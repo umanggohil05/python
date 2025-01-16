78b8f4eab4a4752df6d7072653e366efecf03c48
umanggohil05 <gohilumang1212@gmail.com>
2025-01-16 20:40:30 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,13 @@
+a=float(input("Enter a number:"))
+b=float(input("Enter the another number:"))
+c = a / b
+print(c)
+print(a,b,c)
+print("answer = ",c)
+print(a,"/",b,'=',c)
+
+print(a,b,c,sep="#")
+print(type(a))
+print(type(b))
+print(id(a))
+print(id(b))
