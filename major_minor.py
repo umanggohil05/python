22b0f99acb87705f0767e9c977f6f344688b35bd
umanggohil05 <gohilumang1212@gmail.com>
2025-01-17 12:09:09 +0530

    Add files via upload

--- major_minor.py
+++ major_minor.py
@@ -4,6 +4,7 @@ def major_minor():
        print("you are still minor.")
     else :
         print("You have become major ,now.")
+    print("minor") if age < 18 else print("major")
 
 major_minor()
 major_minor()
