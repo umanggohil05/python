0b0a2e1ca75f83e09f5ad706364513a4dbe59564
umanggohil05 <gohilumang1212@gmail.com>
2025-01-17 11:35:05 +0530

    Add files via upload

---
+++ major_minor.py
@@ -0,0 +1,8 @@
+def major_minor():
+    age = int (input("Enter your age:"))
+    if age<18:
+       print("you are still minor.")
+    else :
+        print("You have become major ,now.")
+
+major_minor()
