d69acddd6023f94e4b089e5939826364dfd472bf
umanggohil05 <gohilumang1212@gmail.com>
2025-01-17 12:31:16 +0530

    Add files via upload

---
+++ largest_smallest out of three.py
@@ -0,0 +1,10 @@
+def largest_smallest():
+    number = int(input("Enter the number:"))
+    if number >0:
+        print("largest")
+    else :
+        print("smallest")
+
+largest_smallest()
+largest_smallest()   
+largest_smallest()   
