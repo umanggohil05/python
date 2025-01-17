e6776677bee74e3b07cfd0aaf9460ea767efadac
umanggohil05 <gohilumang1212@gmail.com>
2025-01-17 12:24:46 +0530

    Add files via upload

---
+++ largest_smallest.py
@@ -0,0 +1,9 @@
+def largest_smallest():
+    number = int(input("Enter the number:"))
+    if number >0:
+        print("largest")
+    else :
+        print("smallest")
+
+largest_smallest()
+largest_smallest()
