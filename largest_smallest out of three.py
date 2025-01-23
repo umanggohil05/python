af708b9321a4544cca22c30c3c7a0e260887077d
umanggohil05 <gohilumang1212@gmail.com>
2025-01-24 04:24:20 +0530

    Update largest_smallest out of three.py

--- largest_smallest out of three.py
+++ largest_smallest out of three.py
@@ -1,10 +1,12 @@
-def largest_smallest():
-    number = int(input("Enter the number:"))
-    if number >0:
-        print("largest")
-    else :
-        print("smallest")
-
-largest_smallest()
-largest_smallest()   
-largest_smallest()   
+a=int(input("Enter the a"))
+b=int(input("Enter the b"))
+c=int(input("Enter the c"))
+print(a)
+print(b)
+print(c)
+if(a>b) and (a>c):
+    print('Largest number is a')
+elif(b>a) and (b>c):
+    print('Largest number is b')
+elif(c>a) and (c>a):
+    print('Largest number is c')
