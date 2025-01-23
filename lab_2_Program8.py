6cd84e549dec81730ce08b8b9197663a55206e95
umanggohil05 <gohilumang1212@gmail.com>
2025-01-24 05:03:47 +0530

    Delete lab_2_Program8.py

--- lab_2_Program8.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter value of angle from 0 to 180:"))
-b=int(input("Enter value of angle from 0 to 180:"))
-c=int(input("Enter value of angle from 0 to 180:"))
-if a+b+c==180 :
-      print("The given angle are forming a triangle")
-else :
-          print("The given angle are not forming a triangle")
