99ebfcd1b6c6f971f1c57c89e476f1f80c115d6d
umanggohil05 <gohilumang1212@gmail.com>
2025-01-24 12:05:44 +0530

    Add files via upload

---
+++ multpy.py
@@ -0,0 +1,13 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> s='pdeu'
+>>> print(s*2)
+pdeupdeu
+>>> 
+========== RESTART: C:/Users/TEMP.CL1-8.003/Desktop/LAB_3/multy.py ==========
+Traceback (most recent call last):
+  File "C:/Users/TEMP.CL1-8.003/Desktop/LAB_3/multy.py", line 5, in <module>
+    sn1()
+NameError: name 'sn1' is not defined
+>>> 
+========== RESTART: C:/Users/TEMP.CL1-8.003/Desktop/LAB_3/multy.py ==========
